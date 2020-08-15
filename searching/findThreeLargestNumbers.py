8d482eaee4264da1719900769fa2237750b42b2f
Stella Liu <46911765+Stella2019@users.noreply.github.com>
2020-08-14 21:50:37 -0400

    Add files via upload

---
+++ searching/findThreeLargestNumbers.py
@@ -0,0 +1,28 @@
+def findThreeLargestNumbers(array):
+    threeLargest = [None, None, None]
+    for num in array:
+        updateLargest(threeLargest, num)
+    return threeLargest
+
+
+def updateLargest(threeLargest, num):
+    if threeLargest[2] is None or num > threeLargest[2]:
+        shiftAndUpdate(threeLargest, num, 2)  # TODO
+    elif threeLargest[1] is None or num > threeLargest[1]:
+        shiftAndUpdate(threeLargest, num, 1)
+    elif threeLargest[0] is None or num > threeLargest[0]:
+        shiftAndUpdate(threeLargest, num, 0)
+
+
+# [x, y, z]
+# [0, 1, 2]
+# [y, z, num]
+# for i in (0, 2+1)
+# if i == 2
+def shiftAndUpdate(array, num, idx):
+    for i in range(idx + 1):
+        if i == idx:
+            array[i] = num
+        else:
+            array[i] = array[i + 1]
+
